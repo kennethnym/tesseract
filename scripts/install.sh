86bb51bd9f3325104b4f3372101be214b9e804d4
Kenneth <kennethnym@hey.com>
2024-12-02 22:55:11 +0000

    chore: add install script

---
+++ scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+arch=$(uname -m)
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  download_url="https://github.com/kennethnym/tesseract/releases/latest/download/tesseract_Linux_$arch.tar.gz"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  download_url="https://github.com/kennethnym/tesseract/releases/latest/download/tesseract_Darwin_$arch.tar.gz"
+else
+  echo "Unsupported OS! tesseract only supports Linux or Darwin."
+  exit 1
+fi
+
+echo "Downloading from $download_url..."
+
+mkdir -p /tmp/tesseract
+curl -L --output /tmp/tesseract/tesseract.tar.gz "$download_url"
+tar -xvzf /tmp/tesseract/tesseract.tar.gz -C /tmp/tesseract
+
+sudo mkdir -p /opt/tesseract
+sudo mv /tmp/tesseract/tesseract /opt/tesseract
+sudo chown $(whoami) /opt/tesseract
+sudo chown $(whoami) /opt/tesseract/*
+sudo cat >/opt/tesseract/config.json <<EOF
+{
+  "port": 80,
+  "databasePath": "./data.sqlite",
+  "hostName": "HOSTNAME"
+}
+EOF
+
+rm -r /tmp/tesseract
+
+echo "tesseract installed successfully to /opt/tesseract."
+echo "Before running tesseract, make sure that you specify the host name in config.json."
